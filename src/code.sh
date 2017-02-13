d8b1d10883276a53f06bafe135c35748378f56f1
aledj2 <aledjones@nhs.net>
2017-02-13 10:29:02 +0000

    variant annotator works.fix #6

--- src/code.sh
+++ src/code.sh
@@ -17,7 +17,7 @@ set -e -x -o pipefail
 #
 # Fetch inputs
 #
-mark-section "downloading inputs"
+#mark-section "downloading inputs"
 #dx-download-all-inputs
 
 dx-download-all-inputs --parallel
@@ -47,7 +47,7 @@ java="java -Xmx$(<.mem_in_mb.txt)m"
 #
 # Detect and download the appropriate human genome and related reference files
 #
-mark-section "detecting reference genome"
+#mark-section "detecting reference genome"
 samtools view -H "$sorted_bam_path" | grep ^@SQ | cut -f1-3 | md5sum | cut -c1-32 >.genome-fingerprint.txt
 case "$(<.genome-fingerprint.txt)" in
   9220d59b0d7a55a43b22cad4a87f6797)
@@ -74,7 +74,7 @@ case "$(<.genome-fingerprint.txt)" in
     ;;
 esac
 
-mark-section "downloading reference genome"
+#mark-section "downloading reference genome"
 APPDATA=project-B6JG85Z2J35vb6Z7pQ9Q02j8
 dx cat "$APPDATA:/misc/gatk_resource_archives/${subgenome}.fasta-index.tar.gz" | tar zxf -
 
@@ -85,7 +85,7 @@ if echo "$sorted_bam_path" | grep -q "WES"; then
   echo "WES Sample - downloading bamfile to parse to HaplotypeCaller"
   if [[ "$vendor_exome" != "" ]]
   then
-    mark-section "downloading exome coordinates"
+    #mark-section "downloading exome coordinates"
     dx download "$APPDATA:/vendor_exomes/${vendor_exome}_${genome}_targets.bed"
     region_opts=("-L" "${vendor_exome}_${genome}_targets.bed")
     if [[ "$padding" != "0" ]]
@@ -100,7 +100,7 @@ fi
 #
 # Fetch GATK resources (these have been prepared in archives)
 #
-mark-section "fetching GATK resources"
+#mark-section "fetching GATK resources"
 dx cat "$APPDATA:/misc/gatk_resource_archives/gatk.resources.${genome}.tar" | tar xf -
 known1="1000G_phase1.indels.${genome}.vcf.gz"
 known2="Mills_and_1000G_gold_standard.indels.${genome}.vcf.gz"
@@ -111,12 +111,12 @@ dbsnp="dbsnp_137.${genome}.vcf.gz"
 #
 if [[ "$skip_markduplicates" != "true" ]]
 then
-  mark-section "marking duplicates"
+  #mark-section "marking duplicates"
   $java -jar picard.jar MarkDuplicates I="$sorted_bam_path" O=deduplicated.bam M=output.metrics CREATE_INDEX=true VALIDATION_STRINGENCY=SILENT $extra_md_options
   rm -f "$sorted_bam_path"
   realign_input="deduplicated.bam"
 else
-  mark-section "indexing input mappings"
+  #mark-section "indexing input mappings"
   samtools index "$sorted_bam_path"
   realign_input="$sorted_bam_path"
 fi
@@ -124,7 +124,7 @@ fi
 #
 # Run GATK indel realignment
 #
-mark-section "realigning indels"
+#mark-section "realigning indels"
 $java -jar GenomeAnalysisTK.jar -nt `nproc` -T RealignerTargetCreator -R genome.fa -I "$realign_input" -o realign.intervals -known $known1 -known $known2 $extra_rtc_options
 $java -jar GenomeAnalysisTK.jar -T IndelRealigner -R genome.fa -I "$realign_input" -targetIntervals realign.intervals -known $known1 -known $known2 -o realigned.bam $extra_ir_options
 rm -f "$realign_input"
@@ -132,7 +132,7 @@ rm -f "$realign_input"
 #
 # Run GATK base recalibration
 #
-mark-section "recalibrating base quality scores"
+#mark-section "recalibrating base quality scores"
 $java -jar GenomeAnalysisTK.jar -nct `nproc` -T BaseRecalibrator -R genome.fa -I realigned.bam -o recal.grp -knownSites $dbsnp -knownSites $known1 -knownSites $known2 $extra_br_options
 $java -jar GenomeAnalysisTK.jar -nct `nproc` -T PrintReads -R genome.fa -I realigned.bam -BQSR recal.grp -o recal.bam $extra_pr_options
 rm -f realigned.bam
@@ -140,29 +140,39 @@ rm -f realigned.bam
 #
 # Run GATK HaplotypeCaller
 #
-mark-section "calling variants"
+#mark-section "calling variants"
 if [[ "$output_format" == "vcf" ]]; then
   $java -jar GenomeAnalysisTK.jar -nct `nproc` -T HaplotypeCaller -R genome.fa -o output.vcf.gz --dbsnp $dbsnp -I recal.bam "${region_opts[@]}" $extra_hc_options
   if [[ ! -e output.vcf.gz.tbi ]]; then
-    mark-section "indexing vcf"
+    #mark-section "indexing vcf"
     tabix -p vcf output.vcf.gz
   fi
 else
   $java -jar GenomeAnalysisTK.jar -nct `nproc` -T HaplotypeCaller -R genome.fa -o output.g.vcf.gz --dbsnp $dbsnp -I recal.bam -ERC GVCF -variant_index_type LINEAR -variant_index_parameter 128000 "${region_opts[@]}" $extra_hc_options
   if [[ ! -e output.g.vcf.gz.tbi ]]; then
-    mark-section "indexing gvcf"
+    #mark-section "indexing gvcf"
     tabix -p vcf output.g.vcf.gz
   fi
   if [[ "$output_format" == "both" ]]; then
     $java -jar GenomeAnalysisTK.jar -nt `nproc` --dbsnp $dbsnp -T GenotypeGVCFs -R genome.fa -o output.vcf.gz -V output.g.vcf.gz $extra_gg_options
     if [[ ! -e output.vcf.gz.tbi ]]; then
-      mark-section "indexing vcf"
+      #mark-section "indexing vcf"
       tabix -p vcf output.vcf.gz
     fi
   fi
 fi
 
-mark-section "uploading results"
+if [[ "$output_format" == "vcf" ]]; then
+tabix -p vcf $inhouse_annotations_path
+  $java -jar GenomeAnalysisTK.jar -T VariantAnnotator -R genome.fa -o output.inhouse.vcf.gz --resource:Inhouse $inhouse_annotations_path --resourceAlleleConcordance -E Inhouse.PreviousClassification -V output.vcf.gz
+  
+  if [[ ! -e output.inhouse.vcf.gz.tbi ]]; then
+    #mark-section "indexing vcf"
+    tabix -p vcf output.inhouse.vcf.gz
+  fi
+fi
+
+#mark-section "uploading results"
 mkdir -p ~/out/bam/output/ ~/out/bai/output/ ~/out/outputmetrics/QC/
 mv recal.bam ~/out/bam/output/"$sorted_bam_prefix".refined.bam
 mv recal.bai ~/out/bai/output/"$sorted_bam_prefix".refined.bam.bai
@@ -170,8 +180,8 @@ mv output.metrics ~/out/outputmetrics/QC/"$sorted_bam_prefix".output.metrics
 
 if [[ "$output_format" != "gvcf" ]]; then
   mkdir -p ~/out/vcf/output/ ~/out/vcf_tbi/output/
-  mv output.vcf.gz ~/out/vcf/output/"$sorted_bam_prefix".vcf.gz
-  mv output.vcf.gz.tbi ~/out/vcf_tbi/output/"$sorted_bam_prefix".vcf.gz.tbi
+  mv output.inhouse.vcf.gz ~/out/vcf/output/"$sorted_bam_prefix".inhouse.vcf.gz
+  mv output.inhouse.vcf.gz.tbi ~/out/vcf_tbi/output/"$sorted_bam_prefix".inhouse.vcf.gz.tbi
 fi
 
 if [[ "$output_format" != "vcf" ]]; then
@@ -184,4 +194,4 @@ fi
 # Upload results
 #
 dx-upload-all-outputs --parallel
-mark-success
+#mark-success
