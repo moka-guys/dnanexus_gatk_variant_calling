ba8cfede2ecb3c5a84d5cb76d0749c51f6ffe25f
aledj2 <aledjones@nhs.net>
2019-05-21 11:32:25 +0100

    remove bed filtering. fix #18 fix #17

--- src/code.sh
+++ src/code.sh
@@ -6,25 +6,14 @@ set -e -x -o pipefail
 
 
 #
-# This app is only a wrapper. The actual GATK3 software must be provided
-# by the user. It is expected to be found in the parent project.
-#
-# Locate and download the GATK jar file.
-#
-#mark-section "locating GATK jar file"
-#download-gatk-jar.sh
-
-#
 # Fetch inputs
 #
-#mark-section "downloading inputs"
-#dx-download-all-inputs
-
+mark-section "downloading inputs"
 dx-download-all-inputs --parallel
 
+# move and rename GATK jar file
 mv ~/in/gatk_jar_file/* ~/GenomeAnalysisTK.jar
 
-# Show all the java versions installed on this worker
 # Show the java version the worker is using
 echo $(java -version)
 
@@ -34,8 +23,6 @@ echo $(java -version)
 #
 update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
 java -jar GenomeAnalysisTK.jar -version || (update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java && java -jar GenomeAnalysisTK.jar -version)
-
-
 
 #
 # Calculate 80% of memory size, for java
@@ -47,7 +34,7 @@ java="java -Xmx$(<.mem_in_mb.txt)m"
 #
 # Detect and download the appropriate human genome and related reference files
 #
-#mark-section "detecting reference genome"
+mark-section "detecting reference genome"
 samtools view -H "$sorted_bam_path" | grep ^@SQ | cut -f1-3 | md5sum | cut -c1-32 >.genome-fingerprint.txt
 case "$(<.genome-fingerprint.txt)" in
   9220d59b0d7a55a43b22cad4a87f6797)
@@ -74,27 +61,30 @@ case "$(<.genome-fingerprint.txt)" in
     ;;
 esac
 
-#mark-section "downloading reference genome"
+mark-section "downloading reference genome"
 APPDATA=project-B6JG85Z2J35vb6Z7pQ9Q02j8
 dx cat "$APPDATA:/misc/gatk_resource_archives/${subgenome}.fasta-index.tar.gz" | tar zxf -
 
 
-# If WES assigne vendor exome bedfile to region_opts
-if echo "$sorted_bam_path" | grep -q 'WES\|Pan493' ; then
-    echo "WES Sample - specifying BED file to use for variant calling by HaplotypeCaller"
-    region_opts=("-L" "/home/dnanexus/in/vendor_exome_bedfile/$vendor_exome_bedfile_prefix.bed")
+# If BED file is given to limit variant calling add to region_opts
+if [[ -e $bedfile_path ]]; then
+    echo "BED file provided to limit variant calling to specified regions"
+    region_opts=("-L" "$bedfile_path")
         if [[ "$padding" != "0" ]]
             then
+                echo "BED file intervals will be padded by $padding bases"
                 region_opts+=("-ip" "$padding")
+        else
+            echo "Padding set to 0 so BED file intervals will not be extended"
         fi
-else 
-    echo "Custom panel Sample no BED file will be passed to HaplotypeCaller"
+else
+    echo "BED file not provided so limit variant calling not restricted. padding argument not relevant"
 fi
 
 #echo $region_opts
 
 # Fetch GATK resources (these have been prepared in archives)
-#mark-section "fetching GATK resources"
+mark-section "fetching GATK resources"
 dx cat "$APPDATA:/misc/gatk_resource_archives/gatk.resources.${genome}.tar" | tar xf -
 known1="1000G_phase1.indels.${genome}.vcf.gz"
 known2="Mills_and_1000G_gold_standard.indels.${genome}.vcf.gz"
@@ -105,12 +95,12 @@ dbsnp="dbsnp_137.${genome}.vcf.gz"
 #
 if [[ "$skip_markduplicates" != "true" ]]
 then
-  #mark-section "marking duplicates"
+  mark-section "marking duplicates - creates indexed sorted BAM"
   $java -jar picard.jar MarkDuplicates I="$sorted_bam_path" O=deduplicated.bam M=output.metrics CREATE_INDEX=true VALIDATION_STRINGENCY=SILENT $extra_md_options
   rm -f "$sorted_bam_path"
   realign_input="deduplicated.bam"
 else
-  #mark-section "indexing input mappings"
+  mark-section "indexing input mappings"
   samtools index "$sorted_bam_path"
   realign_input="$sorted_bam_path"
 fi
@@ -118,7 +108,7 @@ fi
 #
 # Run GATK indel realignment
 #
-#mark-section "realigning indels"
+mark-section "realigning indels"
 $java -jar GenomeAnalysisTK.jar -nt `nproc` -T RealignerTargetCreator -R genome.fa -I "$realign_input" -o realign.intervals -known $known1 -known $known2 $extra_rtc_options
 $java -jar GenomeAnalysisTK.jar -T IndelRealigner -R genome.fa -I "$realign_input" -targetIntervals realign.intervals -known $known1 -known $known2 -o realigned.bam $extra_ir_options
 rm -f "$realign_input"
@@ -126,7 +116,7 @@ rm -f "$realign_input"
 #
 # Run GATK base recalibration
 #
-#mark-section "recalibrating base quality scores"
+mark-section "recalibrating base quality scores"
 $java -jar GenomeAnalysisTK.jar -nct `nproc` -T BaseRecalibrator -R genome.fa -I realigned.bam -o recal.grp -knownSites $dbsnp -knownSites $known1 -knownSites $known2 $extra_br_options
 $java -jar GenomeAnalysisTK.jar -nct `nproc` -T PrintReads -R genome.fa -I realigned.bam -BQSR recal.grp -o recal.bam $extra_pr_options
 rm -f realigned.bam
@@ -134,34 +124,41 @@ rm -f realigned.bam
 #
 # Run GATK HaplotypeCaller
 #
-#mark-section "calling variants"
+## App has options to output VCF, gVCF or both
+# if just VCF
 if [[ "$output_format" == "vcf" ]]; then
   $java -jar GenomeAnalysisTK.jar -nct `nproc` -T HaplotypeCaller -R genome.fa -o output.vcf.gz --dbsnp $dbsnp -I recal.bam "${region_opts[@]}" $extra_hc_options
   if [[ ! -e output.vcf.gz.tbi ]]; then
-    #mark-section "indexing vcf"
+    mark-section "indexing vcf"
     tabix -p vcf output.vcf.gz
   fi
+# if not vcf output gVCF
 else
   $java -jar GenomeAnalysisTK.jar -nct `nproc` -T HaplotypeCaller -R genome.fa -o output.g.vcf.gz --dbsnp $dbsnp -I recal.bam -ERC GVCF -variant_index_type LINEAR -variant_index_parameter 128000 "${region_opts[@]}" $extra_hc_options
   if [[ ! -e output.g.vcf.gz.tbi ]]; then
-    #mark-section "indexing gvcf"
+    mark-section "indexing gvcf"
     tabix -p vcf output.g.vcf.gz
   fi
+  # if output is both repeat, for VCF
   if [[ "$output_format" == "both" ]]; then
     $java -jar GenomeAnalysisTK.jar -nt `nproc` --dbsnp $dbsnp -T GenotypeGVCFs -R genome.fa -o output.vcf.gz -V output.g.vcf.gz $extra_gg_options
     if [[ ! -e output.vcf.gz.tbi ]]; then
-      #mark-section "indexing vcf"
+      mark-section "indexing vcf"
       tabix -p vcf output.vcf.gz
     fi
   fi
 fi
 
 
-#mark-section "uploading results"
+mark-section "uploading results"
 mkdir -p ~/out/bam/output/ ~/out/bai/output/ ~/out/outputmetrics/QC/
 mv recal.bam ~/out/bam/output/"$sorted_bam_prefix".refined.bam
 mv recal.bai ~/out/bai/output/"$sorted_bam_prefix".refined.bam.bai
-mv output.metrics ~/out/outputmetrics/QC/"$sorted_bam_prefix".output.metrics
+# test if mark duplicates is was requested before trying to move mark duplicates output metrics
+if [[ "$skip_markduplicates" != "true" ]]
+then
+    mv output.metrics ~/out/outputmetrics/QC/"$sorted_bam_prefix".output.metrics
+fi
 
 if [[ "$output_format" != "gvcf" ]]; then
   mkdir -p ~/out/vcf/output/ ~/out/vcf_tbi/output/
@@ -179,4 +176,4 @@ fi
 # Upload results
 #
 dx-upload-all-outputs --parallel
-#mark-success
+mark-success
